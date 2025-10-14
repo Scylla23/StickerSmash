b53b00a8705ab2456c451459e5dafbe95d709f86
Pavan Kushnure <2019bcs044@sggs.ac.in>
2025-10-14 20:01:52 +0530

    Expo app init

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  text: {
+    color: "white",
+  },
+});
